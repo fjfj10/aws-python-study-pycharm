e471219b2608f6f4610bfbd700b72b275874544c
fjfj10 <kimmisuk2218@gmail.com>
2023-09-13 17:01:25 +0900

    comment: 주석 추가

--- UserManagement/entity/User.py
+++ UserManagement/entity/User.py
@@ -7,6 +7,7 @@ class User:
         self.name = name
         self.email = email
 
+    # 들어간 정보 확인용(dict로 해도 상관 없음)
     def __str__(self):
         # f""할때 들여쓰기 하지말것 하면 들여쓰기 한 것까지 들어감
         return f"""[User]
